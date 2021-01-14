80d86b01d95dc63fd1994f31e5f27e3ad304ac15
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-01-14 13:50:37 +0000

    Deploy web app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,25 +3,25 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "manifest.json": "a66670d9dce212c5f4ee815430a8acad",
-"assets/NOTICES": "5e8bc907f020030f2993861b0bfeb1f4",
-"assets/assets/play_store.png": "fb481c44958bd1d21e52d0a856286fc4",
-"assets/assets/avatar.png": "efb67af6464aef37a7711002635d43aa",
-"assets/assets/reddit.png": "f062fad2fbb0e11e82eb988d2b0647d1",
-"assets/assets/youtube.png": "eb3072f91cb01f5b1f7c6ac76c404c61",
-"assets/assets/twitter.png": "fba3a2dc663db92a2a02cb4fb1870ad3",
-"assets/assets/web.png": "a98add2dfc3ba7af22c34cec4ad73767",
-"assets/assets/medium.png": "45140ce1eb5fe8d0caed749229873cca",
+  "assets/assets/reddit.png": "f062fad2fbb0e11e82eb988d2b0647d1",
 "assets/assets/github.png": "ef7a02b69836dc8b6a732a54c4200dcb",
+"assets/assets/play_store.png": "fb481c44958bd1d21e52d0a856286fc4",
+"assets/assets/youtube.png": "eb3072f91cb01f5b1f7c6ac76c404c61",
 "assets/assets/stackoverflow.png": "6b99b3bbe6bc99a25625f112a43953bd",
+"assets/assets/medium.png": "45140ce1eb5fe8d0caed749229873cca",
+"assets/assets/web.png": "a98add2dfc3ba7af22c34cec4ad73767",
 "assets/assets/dart_pub.png": "0f2d957830ec863d7d22eb4fc19e76be",
-"assets/AssetManifest.json": "7c03e6fed6e4c3fa8ad3edc957ececa9",
+"assets/assets/avatar.png": "efb67af6464aef37a7711002635d43aa",
+"assets/assets/twitter.png": "fba3a2dc663db92a2a02cb4fb1870ad3",
 "assets/FontManifest.json": "d751713988987e9331980363e24189ce",
-"index.html": "7eb1df50eb9aee69464ed9b32a8037c5",
-"/": "7eb1df50eb9aee69464ed9b32a8037c5",
+"assets/NOTICES": "6a5cb98c2a735c35b1270002ad1e7174",
+"assets/AssetManifest.json": "7c03e6fed6e4c3fa8ad3edc957ececa9",
 "version.json": "426313f2f3133c2f20415344c4a22df3",
-"main.dart.js": "37a65e2ef95c3d1d41c6064f4bd102cb",
-"favicon.png": "f106a5cbc8c59640a851646b83e632d2"
+"favicon.png": "f106a5cbc8c59640a851646b83e632d2",
+"main.dart.js": "694f995db73097a12b4c506bb68135a2",
+"manifest.json": "a66670d9dce212c5f4ee815430a8acad",
+"index.html": "bee5d94dae9232a6a95d1773747b2534",
+"/": "bee5d94dae9232a6a95d1773747b2534"
 };
 
 // The application shell files that are downloaded before a service worker can
