6e890b4610927433a4f3ea669553a497bc5ee938
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-01-14 13:46:47 +0000

    Switch to path URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:portfolio/scaffold.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(const App());
+void main() {
+  setPathUrlStrategy();
+  runApp(const App());
+}
